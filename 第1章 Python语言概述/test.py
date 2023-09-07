b84d172abbf4bba37995e9097521cacaf8ab9ac8
Jackson Ja <Jackson2937703346@163.com>
2023-09-07 09:17:45 +0800

    test

---
+++ 第1章 Python语言概述/test.py
@@ -0,0 +1,13 @@
+# ciphertext = 'ch\at_feY[baUcccQeh'
+# j = 4
+# for i in ciphertext:
+#     print(chr(ord(i) + j), end='')
+#     j += 1
+def main():
+    print("姜美恒沉溺于学习不能自拔！")
+    print("姜美恒非常喜欢Python！")
+    print("姜美恒立志学好Python！")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 第二章 Python基础知识/20230907.py
@@ -0,0 +1,23 @@
+'''if __name__ == '__main__':
+    a = []  # 空列表
+    b = [1, "Hello"]  # 非空列表
+    if a:
+        print(f"{a}True")
+    else:
+        print(f"{a}False")
+    if b:
+        print(f"{b}True")
+    else:
+        print(f"{b}False")
+a = 2e3
+print(a)'''
+
+# if __name__ == '__main__':
+#     a = '''Java
+#     是世界上最好的语言.c'''
+#     print(a)
+
+if __name__ == '__main__':
+    str0 = "Tom said,"
+    str1 = "\"Let\'s go!\""
+    print(str0+str1)
