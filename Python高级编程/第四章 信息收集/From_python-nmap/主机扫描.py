47049f9bc13585f9e2a1de97eaf90adf8f402119
JacksonJa <2937703346@qq.com>
2024-04-12 13:05:02 +0800

    By JacksonJa

--- Python高级编程/第四章 信息收集/From_python-nmap/主机扫描.py
+++ Python高级编程/第四章 信息收集/From_python-nmap/主机扫描.py
@@ -1,10 +1,11 @@
-import nmap
+from nmap import PortScanner
+
 
 if __name__ == '__main__':
     target_host = "127.0.0.1"
     target_port = "1-9999"
     # 创建扫描对象
-    scan_obj = nmap.PortScanner()
+    scan_obj = PortScanner()
     # 调用scan进行扫描
     scan_result = scan_obj.scan(hosts=target_host, ports=target_port, arguments='-sV -O')
     print(scan_result)
