78ca965cbe350c5b796aa6600afd2c3ec83ae371
JacksonJa <2937703346@qq.com>
2024-04-15 16:40:34 +0800

    By JacksonJa

--- Python高级编程/第四章 信息收集/From_python-nmap/主机扫描.py
+++ Python高级编程/第四章 信息收集/From_python-nmap/主机扫描.py
@@ -2,14 +2,20 @@ from nmap import PortScanner
 
 
 if __name__ == '__main__':
-    target_host = "127.0.0.1"
-    target_port = "1-9999"
+    target_host = "192.168.80.0/24"
     # 创建扫描对象
     scan_obj = PortScanner()
     # 调用scan进行扫描
-    scan_result = scan_obj.scan(hosts=target_host, ports=target_port, arguments='-sV -O')
-    print(scan_result)
+    scan_result = scan_obj.scan(hosts=target_host, arguments='-sP')
+    # print(scan_result)  # 整体输出
     # 输出在线主机的IP
+    #   方法一：通过all_hosts()方法提取
+    # up_host_ip = scan_obj.all_hosts()
+    # print(up_host_ip)
+    #   方法二：通过字典方式提取
+    up_host_ip = list(scan_result['scan'].keys())
+    for ip in up_host_ip:
+        print(ip)
     # 输出在线主机的操作系统类型
     # 输出开放的端口
 
