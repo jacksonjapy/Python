e14cc8e9dd62e5e0cd1d32854d6943b64844ddb7
JacksonJa <2937703346@qq.com>
2024-04-15 17:01:00 +0800

    By JacksonJa

--- Python高级编程/第四章 信息收集/From_python-nmap/端口及服务扫描.py
+++ Python高级编程/第四章 信息收集/From_python-nmap/端口及服务扫描.py
@@ -7,13 +7,16 @@ if __name__ == '__main__':
     nm = PortScanner()
     # 调用scan方法进行扫描
     nm.scan(hosts=target_hosts, ports=target_port, arguments="-sS")
-    # 提取在线主机的IP地址以及开放的端口
+    # 提取在线主机的IP地址以及开放的端口（端口号、状态、服务、版本）
     for ip in nm.all_hosts():
         # 判断主机是否有开放的端口
         if len(nm[ip].all_tcp()):
-            print("主机IP地址：", ip)
+            print("主机IP地址：", f"\033[1;32m{ip}\033[0m")
             for port in nm[ip].all_tcp():
-                print(port, end=" ")
+                print(port, nm[ip]["tcp"][port]["state"],
+                      nm[ip]["tcp"][port]["name"],
+                      nm[ip]["tcp"][port]["version"],
+                      sep=" ", end="\n\t")
             print()
         else:
             print(f"{ip}在线，但没有开放端口！")
