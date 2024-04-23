bc9c8641b40efba8b5c27381f8eac4969cf04a64
JacksonJa <2937703346@qq.com>
2024-04-23 16:18:37 +0800

    By JacksonJa

--- Python高级编程/第四章 信息收集/From_scapy/端口扫描.py
+++ Python高级编程/第四章 信息收集/From_scapy/端口扫描.py
@@ -1,5 +1,5 @@
 from scapy.layers.inet import IP, TCP, ICMP, sr1
-from threading import Thread
+from time import time
 
 up_hosts = []
 
@@ -18,9 +18,9 @@ def scapy_host(dst_ip):
     # 3.分析结果，判断主机是否在线
     if answer and answer[ICMP].type == 0 and answer[ICMP].code == 0:
         up_hosts.append(dst_ip)
-        print('host \033[32m %s is online\033[0m' % dst_ip)
+        print('host \033[32m %s \033[0m is online' % dst_ip)
     else:
-        print('host \033[31m %s is offline\033[0m' % dst_ip)
+        print('host \033[31m %s \033[0m is offline' % dst_ip)
 
 
 def scapy_port(dst_ip, dst_port):
@@ -34,11 +34,12 @@ def scapy_port(dst_ip, dst_port):
     answer = sr1(packet, timeout=4, verbose=0)
     # 3.分析结果，判断端口是否开放
     if answer and "TCP" in answer and answer[TCP].flags == "SA":
-        print(dst_port, end="\t")
+        print(dst_port, end=" ")
 
 
 if __name__ == '__main__':
-    target_hosts = ('192.168.80.128', '192.168.80.139', '192.168.80.111')
+    start_time = time()
+    target_hosts = ('192.168.80.128', '192.168.80.4', '192.168.80.111')
     target_ports = (21, 22, 23, 80, 443, 3306, 3307)
     # 一、串行扫描方式
     # 1.扫描主机是否在线
@@ -47,6 +48,8 @@ if __name__ == '__main__':
 
     # 2.对在线主机进行端口扫描
     for ip in up_hosts:  # 每循环一次，就扫描一个主机的端口
-        print('Scanning %s hosts port, open ports have been found:' % ip)
+        print('Scanning %s hosts port, open ports have been found:' % ip, end="")
         for port in target_ports:   # 每循环1次扫描1个端口
             scapy_port(ip, port)
+    end_time = time()
+    print(waste_time := end_time-start_time)
