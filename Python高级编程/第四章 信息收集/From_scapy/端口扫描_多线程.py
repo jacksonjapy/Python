6acc68eb25e5b774c1fd76eda4e1b05dd096a3a6
JacksonJa <2937703346@qq.com>
2024-04-23 16:18:37 +0800

    By JacksonJa

--- Python高级编程/第四章 信息收集/From_scapy/端口扫描_多线程.py
+++ Python高级编程/第四章 信息收集/From_scapy/端口扫描_多线程.py
@@ -1,5 +1,6 @@
 from scapy.layers.inet import IP, TCP, ICMP, sr1
 from threading import Thread
+from time import time
 
 up_hosts = []
 
@@ -34,11 +35,12 @@ def scapy_port(dst_ip, dst_port):
     answer = sr1(packet, timeout=4, verbose=0)
     # 3.分析结果，判断端口是否开放
     if answer and "TCP" in answer and answer[TCP].flags == "SA":
-        print(dst_port, end="\t")
+        print(dst_port, end=" ")
 
 
 if __name__ == '__main__':
-    target_hosts = ('192.168.80.128', '192.168.80.139', '192.168.80.111')
+    start_time = time()
+    target_hosts = ('192.168.80.128', '192.168.80.139', '192.168.80.4')
     target_ports = (21, 22, 23, 80, 443, 3306, 3307)
     # 二、并行扫描方式（多进程）
     threads_list = []
@@ -47,13 +49,20 @@ if __name__ == '__main__':
         host_thread.start()
         threads_list.append(host_thread)
 
+    for thread in threads_list:
+        thread.join()
+
     if len(up_hosts) == 0:  # 判断是否存在在线主机，如果没有主机在线，则不扫描端口。
         pass
     else:
         for up_host in up_hosts:
+            print(f"host {up_host} open port:", end="")
             for port in target_ports:
                 port_thread = Thread(target=scapy_port, args=(up_host, port))
                 port_thread.start()
                 threads_list.append(port_thread)
         for th in threads_list:
             th.join()
+    end_time = time()
+    need_time = (end_time - start_time)
+    print(f"总耗时{need_time:.2f}秒")
