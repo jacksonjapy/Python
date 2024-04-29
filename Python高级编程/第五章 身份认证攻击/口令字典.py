d5032633d4ff7a90747a7d76146898189e85f8c9
JacksonJa <2937703346@qq.com>
2024-04-29 16:13:15 +0800

    By JacksonJa

--- Python高级编程/第五章 身份认证攻击/口令字典.py
+++ Python高级编程/第五章 身份认证攻击/口令字典.py
@@ -7,6 +7,7 @@ def generate_password():
     # 生成一个包含数字或英文小写字母且长度为6位的密码字典文件。
     # 先选一位数字
     # 再选一位英文小写字母
+    # 再选一位特殊字符
     # 剩余位置随机
     while True:
         password_length = int(input("请输入密码长度（密码长度必须大于6）："))
@@ -17,21 +18,19 @@ def generate_password():
             lower_char = combinations(ascii_lowercase, 1)
             special_characters = combinations(punctuation, 1)
             other_char = combinations(ascii_lowercase + digits + punctuation, password_length - 3)
-            lines = 0
+            lines = 100
 
             with open("password.txt", "a+") as file:
                 for digit in digit_char:    # 选取数字
                     for lower in lower_char:    # 选取英文小写字母
                         for special in special_characters:
                             for other in other_char:    # 选取其他字符
-                                if lines < 100:
+                                while lines != 0:
                                     password = list(digit + lower + special + other)
                                     # 打乱密码顺序
                                     shuffle(password)
                                     file.write("".join(password) + "\n")
                                     lines += 1
-                                else:
-                                    break
             print("字典生成成功")
             break
 
