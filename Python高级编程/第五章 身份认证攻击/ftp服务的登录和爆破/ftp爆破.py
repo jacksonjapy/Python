6400ed68812f1254883b0cfbd1cafd88dcedd1d6
JacksonJa <2937703346@qq.com>
2024-04-29 17:19:49 +0800

    By JacksonJa

---
+++ Python高级编程/第五章 身份认证攻击/ftp服务的登录和爆破/ftp爆破.py
@@ -0,0 +1,49 @@
+import socket
+import ftplib
+
+if __name__ == '__main__':
+    # 创建ftp对象
+    ftp = ftplib.FTP()
+    # 与服务器建立连接
+    server = ["192.168.80.4", 21]
+    reply = ftp.connect(server[0], server[1])
+    try:
+        if "220" in reply:
+            print("Connected!")
+        else:
+            print("Connection failed!")
+            exit()
+    except (socket.timeout, ConnectionRefusedError):
+        print("Connection failed, server not found or ftp server not running!")
+        exit()
+    # 登录
+    try:
+        user = ("user1", "123")
+        user_reply = ftp.login(user[0], user[1])
+        if "230" in user_reply:
+            print("Login success!")
+        else:
+            print("Login failed!")
+    except ftplib.error_perm:
+        print("Login failed, username or password mistake!")
+
+    # ftp操作
+    try:
+        while True:
+            cmd = input(">>>")
+            if cmd == "ls":
+                ftp.retrlines("LIST")
+            elif cmd == "pwd":
+                ftp.pwd()
+            elif cmd == "cd":
+                ftp.cwd(input(">>>"))
+            elif cmd == "get":
+                ftp.retrbinary("RETR " + input(">>>"), open(input(">>>"), "wb").write)
+            elif cmd == "put":
+                ftp.storbinary("STOR " + input(">>>"), open(input(">>>"), "rb"))
+            elif cmd == "quit":
+                ftp.quit()
+                break
+    except ftplib.error_perm:
+        print("Operation failed!")
+    # 关闭连接
