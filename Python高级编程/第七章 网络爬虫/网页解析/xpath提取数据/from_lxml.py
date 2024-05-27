7f8d0bd6551401ffe4d3251f22f0385e176d5576
JacksonJa <2937703346@qq.com>
2024-05-27 17:19:58 +0800

    By JacksonJa

--- Python高级编程/第七章 网络爬虫/网页解析/xpath提取数据/from_lxml.py
+++ Python高级编程/第七章 网络爬虫/网页解析/xpath提取数据/from_lxml.py
@@ -1,4 +1,15 @@
-from lxml import etree
+from lxml import html
 
 if __name__ == '__main__':
-    pass
+    html_path = r"./test.html"
+    with open(html_path, "r", encoding="utf-8") as f:
+        html_content = f.read()
+    html_element = html.fromstring(html_content)    # 将字符串转换为Element(HTML)对象
+    useful = html_element.xpath("//div[@class='useful']/ul/li[@class='info']/text()")
+    useless = html_element.xpath("//div[@class='useless']/ul/li[@class='info']/text()")
+    # information = html_element.xpath("/html/head/title/text()")  # 绝对路径
+    for yes in useful:
+        print(yes)
+    print("-" * 11)
+    for no in useless:
+        print(no)
