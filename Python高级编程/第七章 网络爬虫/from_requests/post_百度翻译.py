e7c1c94aa4c4a8f543092ef2ddfecf873133ec88
JacksonJa <2937703346@qq.com>
2024-05-27 16:14:05 +0800

    By JacksonJa

--- Python高级编程/第七章 网络爬虫/from_requests/post_百度翻译.py
+++ Python高级编程/第七章 网络爬虫/from_requests/post_百度翻译.py
@@ -3,7 +3,12 @@ from requests import post
 if __name__ == '__main__':
     url = "https://fanyi.baidu.com/sug"
     while True:
-        source = input("请输入要翻译的文本：")
+        source = input("请输入要翻译的文本(退出请输入exit)：")
+        match source:
+            case "exit":
+                exit(0)
+            case _:
+                pass
         data = {"kw": source}
         response = post(url, data=data)
         if response.status_code == 200:
@@ -11,7 +16,6 @@ if __name__ == '__main__':
             if "data" in result:
                 for item in result["data"]:
                     print(f"原文：{item['k']}\n译文：{item['v']}")
-                break
         else:
             print("请求失败")
             break
