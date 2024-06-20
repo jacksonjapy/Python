2155b814248c5e53bc43a36f042dacfd442075d4
JacksonJa <2937703346@qq.com>
2024-06-20 14:08:48 +0800

    By JacksonJa

--- Python高级编程/第七章 网络爬虫/from_selenim/模拟登录超星.py
+++ Python高级编程/第七章 网络爬虫/from_selenim/模拟登录超星.py
@@ -2,18 +2,46 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from re import search
 
 if __name__ == '__main__':
     target_url = "https://passport2.chaoxing.com/login"
     driver = webdriver.Chrome()
     driver.get(target_url)
 
-    fanyi_id = "18508205503"
-    fanyi_pwd = "jmh+2937703346"
-    user_name = "姜美恒"
+    fanyi_id = ""
+    fanyi_pwd = ""
+    user_name = "李玊"
+
         # WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.ID, "showlogintext")))
-    if login_success := WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, "lg-title"))):
+    if WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, "lg-title"))):
         print("Page open successfully!")
     else:
         print("Page open failed!")
         exit(1)
+
+    id_input = driver.find_element(By.CLASS_NAME, "ipt-tel")  # "//input[@class='ipt-tel']" # 登录框
+    passwd_input = driver.find_element(By.CLASS_NAME, "ipt-pwd")  # "//input[@class='ipt-pwd']" # 密码框
+    login_button = driver.find_element(By.ID, "loginBtn")  # "//button[@id='loginBtn']/text()" # 登录按钮
+    id_input.send_keys(fanyi_id)    # 输入账号
+    passwd_input.send_keys(fanyi_pwd)   # 输入密码
+    login_button.click()
+
+    personalName = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, "personalName"))).text
+    if personalName == user_name:
+        print(f"user:{user_name} Login successfully!")
+    else:
+        print(f"user:{user_name} Login failed!")
+        exit(1)
+
+    error_msg = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, "err-tip"))).text
+    if error_msg == "手机号或密码错误":
+        print("Login failed!")
+        exit(1)
+    else:
+        print("Login failed!")
+
+    course_url = str(driver.find_element(By.ID, "zne_kc_icon").get_attribute("href"))
+    print(course_url)
+    course_url = search(r"[^']*?\)", course_url)
+    input("Press Enter to over...")
